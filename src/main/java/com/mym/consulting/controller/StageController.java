add17b98873a150e12cf36b88392e2cbeb23e7b4
Juan <jfuyo@netgo.net>
2020-03-03 15:19:10 -0500

    add stage

--- src/main/java/com/mym/consulting/controller/StageController.java
+++ src/main/java/com/mym/consulting/controller/StageController.java
@@ -1,11 +1,13 @@
 package com.mym.consulting.controller;
 
 import com.mym.consulting.entities.Etapa;
+import com.mym.consulting.model.Response;
 import com.mym.consulting.model.StageResponse;
 import com.mym.consulting.services.StageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,7 @@ import java.util.List;
 
 @RestController
 public class StageController extends GenericController{
+
     @Autowired
     StageService stageService;
 
@@ -23,6 +26,15 @@ public class StageController extends GenericController{
         logInfo("Inicia consulta de etapas: ");
         List<Etapa> stageList = new ArrayList<Etapa>();
         stageList = stageService.getAllStages();
-        return new ResponseEntity<StageResponse>(new StageResponse("Consulta exitosa", stageList), (stageList != null && !stageList.isEmpty()) ? HttpStatus.OK : HttpStatus.NOT_FOUND);
+        return new ResponseEntity<StageResponse>(new StageResponse("Consulta exitosa", stageList),
+                (stageList != null && !stageList.isEmpty()) ? HttpStatus.OK : HttpStatus.NOT_FOUND);
     }
+
+    @RequestMapping(produces = "application/json", method = RequestMethod.POST, path = "/saveStage")
+    public ResponseEntity<Response> saveProject(@RequestBody(required = true) Etapa etapa){
+        stageService.addStage(etapa);
+        logInfo("Guardando etapa: " + etapa.getNombre());
+        return new ResponseEntity<Response>(Response.getIntance("Etapa guardada exitosamente."), HttpStatus.OK);
+    }
+
 }
